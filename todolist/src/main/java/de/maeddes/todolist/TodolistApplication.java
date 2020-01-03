efbaa684f666fdc3d53a5629cdea4617f68cde8d
Gerry Wildermuth <gerry1313@web.de>
2020-01-03 19:55:01 +0100

    small

--- todolist/src/main/java/de/maeddes/todolist/TodolistApplication.java
+++ todolist/src/main/java/de/maeddes/todolist/TodolistApplication.java
@@ -1,11 +1,15 @@
 package de.maeddes.todolist;
 
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -24,17 +28,63 @@ public class TodolistApplication {
 	@Autowired
 	TodoRepository todoRepository;
 
+	private static Logger logger = LoggerFactory.getLogger("myTodoLogger");
+
+
 	@GetMapping("/todos/")
 	List<String> getTodos() {
 
 		List<String> todos = new ArrayList<String>();
+		int i = 0;
+		int k = 0;
 
+		Random random = new Random();
+		int randomeNumber = random.nextInt(4);
 		// for(Todo todo : todoRepository.findAll()) todos.add(todo.getTodo());
+		logger.info("called TodoExternalService /todos/ the "+i+" time");
+		i++;
+		Random randomGenerator = new Random();
+		int randomInt = randomGenerator.nextInt(6);
+		if (randomInt < 4) {
+			k++;
+			throw new TodoServiceException("Service is the "+k+" time unavailable");
+		}
+
 		todoRepository.findAll().forEach(todo -> todos.add(todo.getTodo()));
 
 		return todos;
 	}
+	@GetMapping("/bulkyTodos/")
+	public List<String> callService() {
+		List<String> todos = new ArrayList<String>();
+		try {
+			int i = 0;
+			logger.info("called BulkyTodoExternalService /bulkyTodos/ the "+i+" time");
+			i++;
+			// Mock processing time of 2 seconds.
+			Thread.sleep(2000);
+			todoRepository.findAll().forEach(todo -> todos.add(todo.getTodo()));
 
+
+			System.out.println(LocalTime.now() + " Call processing finished = " + Thread.currentThread().getName());
+			return todos;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return todos;
+	}
+
+	@SuppressWarnings("serial")
+	class TodoServiceException extends RuntimeException {
+
+		public TodoServiceException(String message) {
+			super(message);
+		}
+		@Override
+		public synchronized Throwable fillInStackTrace() {
+			return this;
+		}
+	}
 	@PostMapping("/todos/{todo}")
 	String addTodo(@PathVariable String todo) {
 
--- todoui/src/main/java/com/example/todoui/TodouiApplication.java
+++ todoui/src/main/java/com/example/todoui/TodouiApplication.java
@@ -1,21 +1,35 @@
 package com.example.todoui;
 
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import io.github.resilience4j.bulkhead.Bulkhead;
+import io.github.resilience4j.bulkhead.BulkheadConfig;
+import io.github.resilience4j.bulkhead.BulkheadRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.http.ResponseEntity;
+import org.springframework.retry.RetryContext;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.PostConstruct;
+import java.lang.reflect.InvocationTargetException;
+import java.time.Duration;
 
 @SpringBootApplication
 @Controller
+@EnableCircuitBreaker
+@EnableRetry
 public class TodouiApplication {
 
 	@Value("${backend.host:localhost}")
@@ -25,6 +39,10 @@ public class TodouiApplication {
 	String backendPort;
 
 	String endpoint;
+
+	private Bulkhead bulkhead;
+
+	int i = 0;
 
 	RestTemplate template = new RestTemplate();
 
@@ -36,27 +54,63 @@ public class TodouiApplication {
 	}
 
 	@GetMapping
-	public String getItems(Model model) {
-
-		System.out.println(" Invoking: " + endpoint + "/todos/");
+	@HystrixCommand(fallbackMethod = "itemsFallback")
+	public String getItems(Model model)
+	{
+		System.out.println(" Invoking: "+endpoint +"/todos/");
 		ResponseEntity<String[]> response = null;
+		response = template.getForEntity(endpoint + "/todos/", String[].class);
 
-		try {
-			
-            response = template.getForEntity(endpoint + "/todos/", String[].class);
+		model.addAttribute("items",response.getBody());
 
-		} catch (Exception e) {
+		return"items";
+	}
 
-			System.out.println(" Backend down" );
-			String[] responseValue = new String[] { "Fix your backend"};
-			model.addAttribute("items", responseValue);
-		}
-
-		if (response != null)
-			model.addAttribute("items", response.getBody());
-
+	public String itemsFallback(Model model) {
+		System.out.println("Fallback called" );
+		String [] responseArray = new String[]{"Fallbackdata","more Fallbackdata","even more Fallbackdata"};
+		model.addAttribute("items",responseArray);
 		return "items";
+	}
 
+	@GetMapping(path = "/Retry")
+	@HystrixCommand
+	@Retryable(value = {Exception.class})
+	public String retryService(Model model) throws Exception{
+		System.out.println(" Invoking: "+endpoint +"/todos/ the "+i+" time from RETRY");
+
+		ResponseEntity<String[]> response = null;
+		response = template.getForEntity(endpoint + "/todos/", String[].class);
+		i++;
+		model.addAttribute("items",response.getBody());
+
+		return"items";
+	}
+
+	@Recover
+	public String itemsRecovery(Model model) {
+		System.out.println("Fallback called" );
+		String [] responseArray = new String[]{"Fallbackdata","more Fallbackdata","even more Fallbackdata"};
+		model.addAttribute("items",responseArray);
+		return "items";
+	}
+	@GetMapping(path = "/Bulky")
+	//@HystrixCommand
+	public String bulkyService(Model model) {
+		System.out.println(" Invoking: "+endpoint +"/todos/ the "+i+" time from RETRY");
+		BulkheadConfig config = BulkheadConfig.custom().maxConcurrentCalls(5).maxWaitDuration(Duration.ofMillis(5000))
+				.build();
+		BulkheadRegistry registry = BulkheadRegistry.of(config);
+
+		bulkhead = registry.bulkhead("externalConcurrentService");
+
+		ResponseEntity<String[]> response = null;
+		Runnable runnable = ()-> template.getForEntity(endpoint + "/bulkyTodos/", String[].class);
+		bulkhead.executeRunnable(runnable);
+		i++;
+		model.addAttribute("items",response.getBody());
+
+		return"items";
 	}
 
 	@PostMapping
